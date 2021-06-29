c30a4d696f239652e4212906154cefd21c056799
shibaji7 <shibaji7@vt.edu>
2021-06-29 17:05:02 -0400

    Generate datasets.

---
+++ src/plotlib.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""plotlib.py: module is dedicated to all plot functions."""
+
+__author__ = "Chakraborty, S."
+__copyright__ = "Copyright 2020, SuperDARN@VT"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+def plot_
---
+++ src/utils.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+"""utils.py: module is dedicated to all utility functions."""
+
+__author__ = "Chakraborty, S."
+__copyright__ = "Copyright 2020, SuperDARN@VT"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0."
+__maintainer__ = "Chakraborty, S."
+__email__ = "shibaji7@vt.edu"
+__status__ = "Research"
+
+import os
+import numpy as np
+
+def create_data_folder_structures(base="../data"):
+    if not os.path.exists(base): os.system("mkdir " + base)
+    if not os.path.exists(base + "/raw"): os.system("mkdir " + base + "/raw")
+    if not os.path.exists(base + "/damp"): os.system("mkdir " + base + "/damp")
+    if not os.path.exists(base + "/train"): os.system("mkdir " + base + "/train")
+    if not os.path.exists(base + "/test"): os.system("mkdir " + base + "/test")
+    if not os.path.exists(base + "/validation"): os.system("mkdir " + base + "/validation")
+    return
+
+def create_plot_folder_structures(base="../plots"):
+    if not os.path.exists(base): os.system("mkdir " + base)
+    if not os.path.exists(base + "/raw"): os.system("mkdir " + base + "/raw")
+    if not os.path.exists(base + "/damp"): os.system("mkdir " + base + "/damp")
+    if not os.path.exists(base + "/train"): os.system("mkdir " + base + "/train")
+    if not os.path.exists(base + "/test"): os.system("mkdir " + base + "/test")
+    if not os.path.exists(base + "/validation"): os.system("mkdir " + base + "/validation")
+    return
+
+def create_backscatter_echoes(base=15, beams=range(4,24), 
+        noise_params={"sigma":1, "mean":0}, seed=0, dur=120):
+    np.random.seed(seed)
+    _o = np.empty((len(beams), dur))
+    for _i, b in enumerate(beams):
+        _o[_i, :] = np.random.normal(loc=base+noise_params["mean"]+(b/np.mean(beams)),
+                scale=noise_params["sigma"], size=dur).astype(int) 
+    return _o
+
+def generate_backscatter_echoes(echo={"loc":20,"scale":5,"samples":10}, 
+        noise={"loc":2,"scale":1,"samples":10}, seed=0, dur=120, beams=range(4,24), 
+        base_dir="../data/raw/", fname="dat_%04d_%03d.txt", clean=True):
+    if clean: os.system("rm -rf " + base_dir + "*")
+    np.random.seed(seed)
+    noise_params_distribution = np.random.gamma(noise["loc"], noise["scale"], noise["samples"])
+    echo_params_distribution = np.random.normal(echo["loc"], echo["scale"], echo["samples"])
+    for n in noise_params_distribution:
+        for e in echo_params_distribution:
+            o = create_backscatter_echoes(base=e, beams=beams,
+                    noise_params={"sigma":n, "mean":0}, seed=seed, dur=dur)
+            print(" Creating data for %d bsc. and %d noise."%(e,n))
+            floc = base_dir + fname%(e,n)
+            np.savetxt(floc, o)
+    return
+
+def create_damp_functions(flare_time_location=30, percentage_drop=1.,
+        drop_rate=3., gain_rate=1e-1, dur=120, seed=0):
+    np.random.seed(seed)
+    fn_sech = lambda x, a0: percentage_drop*(2/(np.exp(a0*x) + np.exp(-a0*x)))
+    xs = np.arange(0, flare_time_location+1) - flare_time_location
+    ys = np.arange(1, dur-flare_time_location)
+    drop = fn_sech(xs, drop_rate)
+    recovery = fn_sech(ys, gain_rate)
+    dmp = 1.-np.round(drop.tolist() + recovery.tolist(),2)
+    return dmp
+
+def generate_backscatter_damp_function(dur=120, seed=0, base_dir="../data/damp/", fname="dmp_dat_%03d_%.2f_%.2f_%.2f.txt", clean=True):
+    if clean: os.system("rm -rf " + base_dir + "*")
+    np.random.seed(seed)
+    flare_time_location_distribution = np.random.uniform(10, 90, 100).astype(int)
+    percentage_drop_ditsribution = np.round(np.random.uniform(0.5, 1., 10), 2)
+    drop_rate_distribution = np.round(np.random.normal(3, .5, 10), 2)
+    gain_rate_distribution = np.round(np.random.uniform(5e-2, 5e-1, 10), 2)
+    for f in flare_time_location_distribution:
+        for p in percentage_drop_ditsribution:
+            for dr in drop_rate_distribution:
+                for gr in gain_rate_distribution:
+                    dmp = create_damp_functions(f, p, dr, gr, dur, seed)
+                    print(" Creating data for %d flare, %.2f percentage rate, %.2f drop rate, and %.2f gain rate."%(f,p,dr,gr))
+                    floc = base_dir + fname%(f,p,dr,gr)
+                    np.savetxt(floc, dmp)
+    return
+
+def create_train_test_dataset(raw_dir="../data/raw/", damp_dir="../data/damp/", train_dir="../data/train/", 
+        test_dir="../data/test/", validation_dir="../data/validation/"):
+
+    return
+
+if __name__ == "__main__":
+    cases = [0, 1, 2, 3]
+    for case in cases:
+        if case==0: create_data_folder_structures()
+        if case==1: create_plot_folder_structures()
+        if case==2: generate_backscatter_echoes()
+        if case==3: generate_backscatter_damp_function()
+    pass
